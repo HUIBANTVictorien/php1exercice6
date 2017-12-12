9384badff3ac6e16d0bc8e91e3eaf0c7968045f9
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 16:08:13 +0100

    maj exo6

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-$nom = 'Victorien';
-echo ('Bonjour '.$nom.', comment vas tu ?');
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 6 partie 1 php</title>
+  </head>
+  <body>
+    <?php
+    $nom = 'Victorien';
+    echo ('Bonjour '.$nom.', comment vas tu ?');
+    ?>
+  </body>
+</html>
